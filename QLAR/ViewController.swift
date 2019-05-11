83784a3680cec3d7a100dd94a928c4a7ab80e4e4
Lary Tertuliano <laryssa.tertuliano@gmail.com>
2019-05-11 08:54:55 -0300

    Initial Commit

---
+++ QLAR/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  QLAR
+//
+//  Created by Lary Tertuliano on 11/05/19.
+//  Copyright © 2019 Lary Tertuliano. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
